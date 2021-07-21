2b5bd160858bb85b171c314a6c2de33626696f80
Nihar <nihars@iitb.ac.in>
2021-07-21 20:10:05 +0530

    Working and tested on the bot. Connection done. Will implement on jetson tomorrow.

--- PythonFiles/ManualControl.py
+++ PythonFiles/ManualControl.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from roboclaw import Roboclaw
 from time import sleep
 address = 0x80
@@ -14,12 +13,14 @@ rc.Open()
 def ForwardFunction(speed):
     if(speed > 127 or speed < 0):
         raise ValueError
-    rc.ForwardMixed(address, speed)
+    rc.ForwardM1(address,speed)
+    rc.ForwardM2(address,speed)
     print("forward w speed " + str(speed))
 def BackwardFunction(speed):
     if(speed > 127 or speed < 0):
         raise ValueError
-    rc.BackwardMixed(address, speed)
+    rc.BackwardM1(address,speed)
+    rc.BackwardM2(address,speed)
     print("Back w speed " + str(speed))
 def encoder_test():
     print("This will move robot please confirm we are clear to move")
@@ -28,25 +29,29 @@ def encoder_test():
         #move motor forward for some time
         rc.SetEncM1(address,0)
         rc.SetEncM2(address,0)
-        rc.ForwardMixed(address,64)
+        rc.ForwardM1(address,64)
+        rc.ForwardM2(address,64)
         # delay(d_val)
         sleep(1)
-        rc.ForwardMixed(address,0)
+        rc.ForwardM1(address,0)
+        rc.ForwardM2(address,0)
         # stop()
         en1=rc.ReadEncM1(address)
         en2=rc.ReadEncM2(address)
         print("forward: enc1={}; enc2={}".format(en1,en2))
-        rc.BackwardMixed(address,64)
+        rc.BackwardM1(address,64)
+        rc.BackwardM2(address,64)
         # delay(2*d_val)
         sleep(2)
-        rc.ForwardMixed(address,0)
+        rc.ForwardM1(address,0)
+        rc.ForwardM2(address,0)
         # stop()
         en3=rc.ReadEncM1(address)
         en4=rc.ReadEncM2(address)
         print("backward: enc1={}; enc2={}".format(en3,en4))
-        if(en1*en3<0 and en2*en4<0 and en3*en4>0 and en2*en1>0):
+        if(en1[1]*en3[1]<0 and en2[1]*en4[1]<0 and en3[1]*en4[1]>0 and en2[1]*en1[1]>0):
             print("encoder test done---working fine")
-            # return True
+            return True
         else:
     	    print("Error in directions of the motor , please reconnect the wires and tune again!!!")
             raise ValueError
@@ -62,8 +67,27 @@ def right(speed):
     rc.TurnRightMixed(address,speed)
     print("Right")
 def stop():
-    rc.ForwardMixed(address,0)
+    rc.ForwardM1(address,0)
+    rc.ForwardM2(address,0)
     print("stopped")
+def connection_test():
+    print("This will move robot please confirm we are clear to move")
+    if(raw_input("[y/n]").lower()=="y"):
+        print("Connection_test")
+        rc.SetEncM1(address,0)
+        rc.SetEncM2(address,0)
+        rc.ForwardM1(address,30)
+        sleep(0.5)
+        c = rc.ReadEncM1(address)
+        if (c[1]> 30 or c[1] < -30):
+            print("M1 OK! {}".format(c))    
+        rc.ForwardM1(address,0)
+        rc.ForwardM2(address,30)
+        sleep(0.5)
+        c = rc.ReadEncM2(address)
+        if (c[1]> 30 or c[1] < -30):
+            print("M2 OK! {}".format(c))    
+        rc.ForwardM2(address,0)
 speed = 0
 #----------------Main Function -------------------#
 print("#########################")
@@ -82,11 +106,8 @@ while 1:
             print("Raw_input variable = " + str(r_char[1:]))
 #------------------Main Functions -----------------------------#            
         if(r_char[0] == "F"):
-            # try:
             speed = int(r_char[1:])
             ForwardFunction(speed)
-            # except:
-                # print("Speed Missing or incorrect")
         if(r_char[0] == "B"):
             speed = int(r_char[1:])
             BackwardFunction(speed)
@@ -100,6 +121,8 @@ while 1:
             right(speed)
         if(r_char[0] == "S"):
             stop()
+        if(r_char[0] == "C"):
+            connection_test()
  #-------------Exception Handling-------------------#   
     except KeyboardInterrupt:
         print("\nExiting....")
@@ -110,119 +133,3 @@ while 1:
         print("Port Not opened")
     except:
         print("Invalid Input")
-=======
-from roboclaw import Roboclaw
-import math as m
-address = 0x80
-debug = 0
-d_val=200
-rc = Roboclaw("/dev/ttyACM0",115200)
-rc.Open()
-
-#------------------Functions----------------------
-def delay(val=1000):
-    for i in range(0,val):
-        pass
-	 
-def ForwardFunction(speed):
-    if(speed > 127 or speed < 0):
-        raise ValueError
-    rc.ForwardMixed(address, speed)
-    print("forward w speed " + str(speed))
-def BackwardFunction(speed):
-    if(speed > 127 or speed < 0):
-        raise ValueError
-    rc.BackwardMixed(address, speed)
-    print("Back w speed " + str(speed))
-def encoder_test(speed,d_val):
-    print("This will move robot please confirm we are clear to move")
-    if(raw_input("[y/n]").lower()=="y"):
-        print("encoder_test")
-        
-    #move motor forward for some time
-        SetEncM1(self,address,0):
-        SetEncM2(self,address,0):
-        rc.ForwardMixed(address,speed)
-        delay(d_val)
-        rc.stop()
-        en1=rc.ReadEncM1(address)
-        en2=rc.ReadEncM2(address)
-        print("forward: enc1={}; enc2={}".format(en1,en2))
-        rc.BackwardMixed(address,speed)
-        delay(2*d_val)
-        rc.stop()
-        en3=rc.ReadEncM1(address)
-        en4=rc.ReadEncM2(address)
-        print("backward: enc1={}; enc2={}".format(en3,en4))
-        if(en1*en3<0 and en2*en4<0 and en3*en4>0 and en2*en1>0)
-        print("encoder test done---working fine")
-            return True
-        else
-    	
-            print("Error in directions of the motor , please reconnect the wires and tune again!!!")
-            raise ValueError
-    
-  
-    	
-        #here
-
-def left(speed):
-    if(speed > 127 or speed < 0):
-        raise ValueError
-    rc.TurnLeftMixed(address,speed)
-    print("Left")
-def right(speed):
-    if(speed > 127 or speed < 0):
-        raise ValueError
-    rc.TurnRightMixed(address,speed)
-    print("Right")
-def stop():
-    rc.ForwardMixed(address,0)
-    print("stopped")
-speed = 0
-#----------------Main Function -------------------#
-print("#########################")
-print("Manual Control on jetson nano")
-print("please run test to check encoder connection, Case insensitive")
-print("Fxxx: Forward w speed xxx ")
-print("Bxxx: Backward w speed xxx ")
-print("L: Left \t R: Right")
-print("E: Encoder test")
-while 1:
-    try:
-        r_char = raw_input("Please Enter your input: ")
-        r_char = r_char.upper()
-        if (debug == 1):
-            print("Raw_input time = " +  str(r_char[0]))
-            print("Raw_input variable = " + str(r_char[1:]))
-#------------------Main Functions -----------------------------#            
-        if(r_char[0] == "F"):
-            # try:
-            speed = int(r_char[1:])
-            ForwardFunction(speed)
-            # except:
-                # print("Speed Missing or incorrect")
-        if(r_char[0] == "B"):
-            speed = int(r_char[1:])
-            BackwardFunction(speed)
-        if(r_char[0] == "E"):
-            encoder_test(speed,d_val)
-        if(r_char[0] == "L"):
-            speed = int(r_char[1:])
-            left(speed);
-        if(r_char[0] == "R"):
-            speed = int(r_char[1:])
-            right(speed)
-        if(r_char[0] == "S"):
-            stop()
- #-------------Exception Handling-------------------#   
-    except KeyboardInterrupt:
-        print("\nExiting....")
-        break
-    except ValueError:
-        print("Speed Missing or Incorrect")
-    except AttributeError:
-        print("Port Not opened")
-    except:
-        print("Invalid Input")
->>>>>>> 97e91a81b7b7f5ff94d1230acd5d574ed8127d32
