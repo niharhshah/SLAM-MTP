97e91a81b7b7f5ff94d1230acd5d574ed8127d32
arjunraj1995237 <rjmeansarjun@gmail.com>
2021-07-21 13:05:47 +0530

    encoder test code added

--- PythonFiles/ManualControl.py
+++ PythonFiles/ManualControl.py
@@ -1,10 +1,16 @@
 from roboclaw import Roboclaw
+import math as m
 address = 0x80
 debug = 0
+d_val=200
 rc = Roboclaw("/dev/ttyACM0",115200)
 rc.Open()
 
 #------------------Functions----------------------
+def delay(val=1000):
+    for i in range(0,val):
+        pass
+	 
 def ForwardFunction(speed):
     if(speed > 127 or speed < 0):
         raise ValueError
@@ -15,10 +21,36 @@ def BackwardFunction(speed):
         raise ValueError
     rc.BackwardMixed(address, speed)
     print("Back w speed " + str(speed))
-def encoder_test():
+def encoder_test(speed,d_val):
     print("This will move robot please confirm we are clear to move")
     if(raw_input("[y/n]").lower()=="y"):
         print("encoder_test")
+        
+    #move motor forward for some time
+        SetEncM1(self,address,0):
+        SetEncM2(self,address,0):
+        rc.ForwardMixed(address,speed)
+        delay(d_val)
+        rc.stop()
+        en1=rc.ReadEncM1(address)
+        en2=rc.ReadEncM2(address)
+        print("forward: enc1={}; enc2={}".format(en1,en2))
+        rc.BackwardMixed(address,speed)
+        delay(2*d_val)
+        rc.stop()
+        en3=rc.ReadEncM1(address)
+        en4=rc.ReadEncM2(address)
+        print("backward: enc1={}; enc2={}".format(en3,en4))
+        if(en1*en3<0 and en2*en4<0 and en3*en4>0 and en2*en1>0)
+        print("encoder test done---working fine")
+            return True
+        else
+    	
+            print("Error in directions of the motor , please reconnect the wires and tune again!!!")
+            raise ValueError
+    
+  
+    	
         #here
 
 def left(speed):
@@ -29,7 +61,7 @@ def left(speed):
 def right(speed):
     if(speed > 127 or speed < 0):
         raise ValueError
-    rc.TurnRightMixed(address,speed);
+    rc.TurnRightMixed(address,speed)
     print("Right")
 def stop():
     rc.ForwardMixed(address,0)
@@ -61,7 +93,7 @@ while 1:
             speed = int(r_char[1:])
             BackwardFunction(speed)
         if(r_char[0] == "E"):
-            encoder_test()
+            encoder_test(speed,d_val)
         if(r_char[0] == "L"):
             speed = int(r_char[1:])
             left(speed);
